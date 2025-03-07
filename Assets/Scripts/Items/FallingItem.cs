32f8873576c419a8f662524992ea7606c5bb5c29
Joseph <ziyoyevyusuf@gmail.com>
2025-03-08 01:47:37 +0500

    Added falling item and trigger

---
+++ Assets/Scripts/Items/FallingItem.cs
@@ -0,0 +1,50 @@
+﻿using Triggers;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Items
+{
+	[RequireComponent(typeof(AudioSource))]
+	public class FallingItem : MonoBehaviour
+	{
+		[SerializeField] private PlayerTrigger _trigger;
+		[SerializeField] private Transform _fallTransform;
+		[SerializeField] private float _fallChance = 100;
+		
+		private AudioSource _audioSource;
+
+		private void Awake()
+		{
+			_audioSource = GetComponent<AudioSource>();
+		}
+
+		private void OnEnable()
+		{
+			_trigger.Triggered += Fall;
+		}
+
+		private void OnDisable()
+		{
+			_trigger.Triggered -= Fall;
+		}
+
+		private void Fall()
+		{
+			if (transform.position == _fallTransform.position) return;
+
+			if (!RandomChance()) return;
+			
+			transform.position = _fallTransform.position;
+			transform.rotation = _fallTransform.rotation;
+			
+			_audioSource.Play();
+		}
+
+		private bool RandomChance()
+		{
+			int value = Random.Range(0, 100);
+
+			return value <= _fallChance;
+		}
+	}
+}
---
+++ Assets/Scripts/Triggers/PlayerTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Triggers
+{
+	[RequireComponent(typeof(Collider))]
+	public class PlayerTrigger : MonoBehaviour
+	{
+		public event UnityAction Triggered; 
+		private void OnTriggerEnter(Collider other)
+		{
+			if (!other.CompareTag("Player")) return;
+
+			Triggered?.Invoke();
+		}
+	}
+}
