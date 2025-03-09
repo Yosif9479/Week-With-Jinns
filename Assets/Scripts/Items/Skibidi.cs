867c8776ddc523ecd8421632f47533d50d8a6a5b
Joseph <ziyoyevyusuf@gmail.com>
2025-03-09 21:52:19 +0500

    Added skibidi. Added occlusion culling

---
+++ Assets/Scripts/Items/Skibidi.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using Interfaces;
+using UnityEngine;
+
+namespace Items
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class Skibidi : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private float _flushDurationSeconds;
+        [SerializeField] private float _flushRefillSeconds;
+        [SerializeField] private GameObject _water;
+        
+        private AudioSource _audioSource;
+        private bool _isFlushing;
+        private bool _isRefilling;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void Interact()
+        {
+            if (_isFlushing || _isRefilling) return;
+            
+            _water.SetActive(true);
+            _audioSource.Play();
+            _isFlushing = true;
+            
+            StartCoroutine(StopFlushing());
+        }
+
+        private IEnumerator StopFlushing()
+        {
+            yield return new WaitForSeconds(_flushDurationSeconds);
+            _water.SetActive(false);
+            _isFlushing = false;
+            _isRefilling = true;
+            StartCoroutine(StopRefilling());
+        }
+
+        private IEnumerator StopRefilling()
+        {
+            yield return new WaitForSeconds(_flushRefillSeconds);
+            _isRefilling = false;
+            _audioSource.Stop();
+        }
+    }
+}
