0474b54e8bbc8f2c1d072e455c2f8821440770d7
Joseph <ziyoyevyusuf@gmail.com>
2025-03-11 04:05:06 +0500

    Removed occlusion culling. Made doors open rotating and not instantly

--- Assets/Scripts/Items/Door.cs
+++ Assets/Scripts/Items/Door.cs
@@ -1,12 +1,14 @@
 ﻿using Interfaces;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Items
 {
     [RequireComponent(typeof(AudioSource))]
     public class Door : MonoBehaviour, IInteractable
     {
-        [SerializeField] private bool _openOnStart;
+        [SerializeField] private bool _openByDefault;
+        [SerializeField] private float _rotationTimeSeconds = 1f;
         
         [Header("Audio")]
         [SerializeField] private AudioClip _openSound;
@@ -16,6 +18,8 @@ namespace Items
         private Vector3 _openRotation;
         
         private AudioSource _audioSource;
+        private Vector3 _targetRotation;
+        private bool _isRotating;
 
         private void Awake()
         {
@@ -30,18 +34,36 @@ namespace Items
             if (_closedRotation.y >= 360) _closedRotation.y -= 360;
             if (_openRotation.y >= 360) _openRotation.y -= 360;
             
-            if (_openOnStart) transform.localEulerAngles = _openRotation;
+            if (_openByDefault) transform.localEulerAngles = _openRotation;
+        }
+
+        private void FixedUpdate()
+        {
+            if (_isRotating) Rotate();
         }
         
         public void Interact()
         {
+            if (_isRotating) return;
+            
             bool isOpen = Mathf.Approximately(transform.localEulerAngles.y, _openRotation.y);
             
             Vector3 rotation = isOpen ? _closedRotation : _openRotation;
             
-            transform.localEulerAngles = rotation;
+            _targetRotation = rotation;
+
+            _isRotating = true;
 
             _audioSource.PlayOneShot(isOpen ? _openSound : _closeSound);
+        }
+
+        private void Rotate()
+        {
+            Quaternion rotation = Quaternion.Euler(_targetRotation);
+            
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, rotation, 90 / _rotationTimeSeconds * Time.fixedDeltaTime);
+
+            if (Mathf.Approximately(transform.localEulerAngles.y, _targetRotation.y)) _isRotating = false;
         }
     }
 }
