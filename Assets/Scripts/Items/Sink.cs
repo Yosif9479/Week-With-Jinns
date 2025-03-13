c377f23790f8041d72f8353e5ec15c45fab4d01c
Joseph <ziyoyevyusuf@gmail.com>
2025-03-13 19:34:41 +0500

    Added cup. Changed sink

---
+++ Assets/Scripts/Items/Sink.cs
@@ -0,0 +1,28 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace Items
+{
+    public class Sink : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private GameObject _water;
+        [SerializeField] private Transform _handleTransform;
+
+        private Quaternion _handleClosedRotation;
+        [SerializeField] private Quaternion _handleOpenRotation;
+
+        private void Start()
+        {
+            _handleClosedRotation = _handleTransform.localRotation;
+        }
+        
+        public void Interact()
+        {
+            _water.SetActive(!_water.activeInHierarchy);
+            
+            bool isOpen = _water.activeInHierarchy;
+            
+            _handleTransform.localRotation =  isOpen ? _handleOpenRotation : _handleClosedRotation;
+        }
+    }
+}
