8048962c899602fc8a5a908f869e6d0ee6ceac6a
Joseph <ziyoyevyusuf@gmail.com>
2025-03-25 08:26:08 +0500

    Added collision sound system. Added temporary Pan collision sound

---
+++ Assets/Scripts/ItemScripts/CollisionSound.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Models;
+using UnityEngine;
+
+namespace ItemScripts
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class CollisionSound : MonoBehaviour
+    {
+        [SerializeField] private ThresholdToSound[] _thresholds = {};
+        
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            ThresholdToSound threshold = _thresholds.LastOrDefault(x => x.Threshold <= collision.relativeVelocity.magnitude);
+
+            if (threshold == null) return;
+            
+            _audioSource.PlayOneShot(threshold.Clip);
+        }
+
+        private void Reset()
+        {
+            var audioSource = GetComponent<AudioSource>();
+
+            if (_thresholds.Length == 0)
+            {
+                _thresholds = new ThresholdToSound[]
+                {
+                    new() { Threshold = 1, Clip = audioSource.clip }
+                };
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Models/ThresholdToSound.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Models
+{
+    [Serializable]
+    public class ThresholdToSound
+    {
+        public float Threshold;
+        public AudioClip Clip;
+    }
+}
